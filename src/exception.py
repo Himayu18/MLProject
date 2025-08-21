2b2033baf4bf5e45b7cbba9989edef4ddc8b8e4d
Himayu18 <himayudhoke18@gmail.com>
2025-08-21 20:57:20 +0530

    Logging and Exception 2

--- src/exception.py
+++ src/exception.py
@@ -13,4 +13,5 @@ class CustomException(Exception):
         super().__init__(error_message)
         self.error_message = error_message_details(error_message,error_detail=error_detail)
 
-   
+    def __str__(self):
+        return self.error_message 
